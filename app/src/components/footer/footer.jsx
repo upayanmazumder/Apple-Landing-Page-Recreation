66c7e31d171f8d8ec8cbdfcef048dc2d4bafa96a
Upayan Mazumder <upayanm3@gmail.com>
2025-02-13 17:43:51 +0530

    Add detailed footer sections for Apple services and improve styles

--- app/src/components/footer/footer.jsx
+++ app/src/components/footer/footer.jsx
@@ -58,7 +58,190 @@ const Footer = () => {
                     <li>
                         <a href="#">Store</a>
                     </li>
-                    
+                    <li>
+                        <h3>
+                            Apple Wallet
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Wallet</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            Account
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Manage Your Apple Account</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Store Account</a>
+                    </li>
+                    <li>
+                        <a href="#">iCloud.com</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            Entertainment
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Apple One</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple TV+</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Music</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Arcade</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Podcasts</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Books</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Store</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            Apple Store
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Find a Store</a>
+                    </li>
+                    <li>
+                        <a href="#">Genius Bar</a>
+                    </li>
+                    <li>
+                        <a href="#">Today at Apple</a>
+                    </li>
+                    <li>
+                        <a href="#">Group Reservations</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Camp</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Trade In</a>
+                    </li>
+                    <li>
+                        <a href="#">Ways to Buy</a>
+                    </li>
+                    <li>
+                        <a href="#">Recycling Programme</a>
+                    </li>
+                    <li>
+                        <a href="#">Order Status</a>
+                    </li>
+                    <li>
+                        <a href="#">Shopping Help</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            For Business
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Apple and Business</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop for Business</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            For Education
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Apple and Education</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop for Education</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop for University</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            For Healthcare
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Apple in Healthcare</a>
+                    </li>
+                    <li>
+                        <a href="#">Mac in Healthcare</a>
+                    </li>
+                    <li>
+                        <a href="#">Health on Apple Watch</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            Apple Values
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Accessibility</a>
+                    </li>
+                    <li>
+                        <a href="#">Education</a>
+                    </li>
+                    <li>
+                        <a href="#">Environment</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy</a>
+                    </li>
+                    <li>
+                        <a href="#">Supply Chain</a>
+                    </li>
+                </ul>
+                <ul className={footerStyles.column}>
+                    <li>
+                        <h3>
+                            About Apple
+                        </h3>
+                    </li>
+                    <li>
+                        <a href="#">Newsroom</a>
+                    </li>
+                    <li>
+                        <a href="#">Apple Leadership</a>
+                    </li>
+                    <li>
+                        <a href="#">Career Oppurtunities</a>
+                    </li>
+                    <li>
+                        <a href="#">Investors</a>
+                    </li>
+                    <li>
+                        <a href="#">Ethics and Compliance</a>
+                    </li>
+                    <li>
+                        <a href="#">Events</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Apple</a>
+                    </li>
                 </ul>
             </div>
             <div className={footerStyles.footNote}>
