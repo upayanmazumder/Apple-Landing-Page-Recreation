4f90662b78102a2c56ccf41fc16c78b5de1c5a9b
Upayan Mazumder <upayanm3@gmail.com>
2025-02-13 22:51:01 +0530

    Refactor header component to implement responsive menu toggle; enhance styles for better layout and accessibility

--- app/src/components/header/header.jsx
+++ app/src/components/header/header.jsx
@@ -1,80 +1,45 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import headerStyles from "./header.module.css";
-import { BsApple, BsSearch, BsBag} from "react-icons/bs";
+import { BsApple, BsSearch, BsBag, BsList, BsX } from "react-icons/bs";
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header className={headerStyles.header}>
             <div className={headerStyles.content}>
-                <nav className={headerStyles.nav}>
+                <div className={headerStyles.logo}>
+                    <a href="/">
+                        <BsApple className={headerStyles.icon}/>
+                    </a>
+                </div>
+                <button className={headerStyles.menuToggle} onClick={toggleMenu}>
+                    {isMenuOpen ? <BsX /> : <BsList />}
+                </button>
+                <nav className={`${headerStyles.nav} ${isMenuOpen ? headerStyles.open : ''}`}>
                     <ul>
-                        <div className={headerStyles.logo}>
-                            <a href="/">
-                                <BsApple className={headerStyles.icon}/>
-                            </a>
-                        </div>
-                        <li>
-                            <a href="#store">
-                                Store
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#mac">
-                                Mac
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#ipad">
-                                iPad
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#iphone">
-                                iPhone
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#watch">
-                                Watch
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#airpods">
-                                AirPods
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#tv-home">
-                                TV & Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#entertainment">
-                                Entertainment
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#accessories">
-                                Accessories
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#support">
-                                Support
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#search"> 
-                                <BsSearch />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#cart"> 
-                                <BsBag />
-                            </a>
-                        </li>
+                        <li><a href="#store">Store</a></li>
+                        <li><a href="#mac">Mac</a></li>
+                        <li><a href="#ipad">iPad</a></li>
+                        <li><a href="#iphone">iPhone</a></li>
+                        <li><a href="#watch">Watch</a></li>
+                        <li><a href="#airpods">AirPods</a></li>
+                        <li><a href="#tv-home">TV & Home</a></li>
+                        <li><a href="#entertainment">Entertainment</a></li>
+                        <li><a href="#accessories">Accessories</a></li>
+                        <li><a href="#support">Support</a></li>
                     </ul>
                 </nav>
+                <div className={headerStyles.icons}>
+                    <a href="#search"><BsSearch /></a>
+                    <a href="#cart"><BsBag /></a>
+                </div>
             </div>
         </header>
     );
