229ec34cd610e586c4cd4ee43bbcc652a64a0ce3
Upayan Mazumder <upayanm3@gmail.com>
2025-02-13 16:00:37 +0530

    Remove unused font imports and simplify body class in layout component

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Header from "../components/header/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Apple",
@@ -21,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Header />
         {children}
       </body>
