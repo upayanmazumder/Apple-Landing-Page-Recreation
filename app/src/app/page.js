ea4468960781d7eadf5da48874e6362720e8c891
Upayan Mazumder <upayanm3@gmail.com>
2025-02-13 16:58:51 +0530

    Add Apple Watch Series 10 section with images and buttons; update iPhone 16 Pro details

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import { BsApple } from "react-icons/bs";
 import LearnMoreButton from "../components/landing/ui/buttons/learn-more/learn-more";
 import BuyButton from "../components/landing/ui/buttons/buy/buy";
 import Shop from "../components/landing/shop/shop";
@@ -22,6 +23,54 @@ export default function Home() {
         >
           <Image
             src="/landing/hero-iphone16.jpg"
+            alt="iPhone 16"
+            fill
+            priority
+            style={{
+              objectFit: 'cover',
+              objectPosition: 'center',
+            }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              top: '10%',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              textAlign: 'center',
+              color: 'white',
+            }}
+          >
+            <h1 className={pageStyles.heading}>iPhone 16 Pro</h1>
+            <h2 className={pageStyles.subHeading}>Built for Apple Intelligence.</h2>
+          </div>
+          <div
+            style={{
+              position: 'absolute',
+              bottom: '10%',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              display: 'flex',
+              gap: '20px',
+            }}
+          >
+            <LearnMoreButton />
+            <BuyButton />
+          </div>
+        </a>
+      </section>
+      <section>
+        <a
+          href="/iphone-16-pro"
+          style={{
+            display: 'block',
+            position: 'relative',
+            width: '100%',
+            height: '90vh',
+          }}
+        >
+          <Image
+            src="/landing/iphone16-large-tall-blue.jpg"
             alt="iPhone 16 Pro"
             fill
             priority
@@ -69,7 +118,7 @@ export default function Home() {
           }}
         >
           <Image
-            src="/landing/iphone15-large-tall-blue.jpg"
+            src="/landing/hero_apple_watch_series_10.jpg"
             alt="iPhone 16 Pro"
             fill
             priority
@@ -88,21 +137,27 @@ export default function Home() {
               color: 'white',
             }}
           >
-            <h1 className={pageStyles.heading}>iPhone 16</h1>
-            <h2 className={pageStyles.subHeading}>Built for Apple Intelligence.</h2>
-          </div>
-          <div
-            style={{
-              position: 'absolute',
-              bottom: '10%',
-              left: '50%',
-              transform: 'translateX(-50%)',
-              display: 'flex',
-              gap: '20px',
-            }}
-          >
-            <LearnMoreButton />
-            <BuyButton />
+            <h1 className={pageStyles.heading} style={{"color" : "black"}}>
+              <BsApple /> WATCH
+            </h1>
+            <h2 className={pageStyles.subHeading} style={{"color" : "black"}}>
+              SERIES 10 
+              <br />
+              Thinstant Classic
+            </h2>
+            <div
+              style={{
+                position: 'absolute',
+                top: '110%',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                display: 'flex',
+                gap: '20px',
+              }}
+            >
+              <LearnMoreButton />
+              <BuyButton />
+            </div>
           </div>
         </a>
       </section>
