9633ecae2d66f3106c9014d2df1649bd757dc003
Upayan Mazumder <upayanm3@gmail.com>
2025-02-13 23:00:52 +0530

    Refactor footer styles for improved layout and responsiveness; enhance accessibility features

--- app/src/components/footer/footer.jsx
+++ app/src/components/footer/footer.jsx
@@ -26,23 +26,23 @@ const Footer = () => {
                     <details open>
                         <summary className={footerStyles.heading}>Shop and Learn</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Store</a></li>
-                            <li className={footerStyles.link}><a href="#">Mac</a></li>
-                            <li className={footerStyles.link}><a href="#">iPad</a></li>
-                            <li className={footerStyles.link}><a href="#">iPhone</a></li>
-                            <li className={footerStyles.link}><a href="#">AirPods</a></li>
-                            <li className={footerStyles.link}><a href="#">TV & Home</a></li>
-                            <li className={footerStyles.link}><a href="#">AirTag</a></li>
-                            <li className={footerStyles.link}><a href="#">Accessories</a></li>
-                            <li className={footerStyles.link}><a href="#">Gift Cards</a></li>
-                            <li className={footerStyles.link}><a href="#">Store</a></li>
+                            <li className={footerStyles.link}><a>Store</a></li>
+                            <li className={footerStyles.link}><a>Mac</a></li>
+                            <li className={footerStyles.link}><a>iPad</a></li>
+                            <li className={footerStyles.link}><a>iPhone</a></li>
+                            <li className={footerStyles.link}><a>AirPods</a></li>
+                            <li className={footerStyles.link}><a>TV & Home</a></li>
+                            <li className={footerStyles.link}><a>AirTag</a></li>
+                            <li className={footerStyles.link}><a>Accessories</a></li>
+                            <li className={footerStyles.link}><a>Gift Cards</a></li>
+                            <li className={footerStyles.link}><a>Store</a></li>
                         </ul>
                     </details>
 
                     <details open>
                         <summary className={footerStyles.heading}>Apple Wallet</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Wallet</a></li>
+                            <li className={footerStyles.link}><a>Wallet</a></li>
                         </ul>
                     </details>
                 </div>
@@ -51,22 +51,22 @@ const Footer = () => {
                     <details open>
                         <summary className={footerStyles.heading}>Account</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Manage Your Apple Account</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Store Account</a></li>
-                            <li className={footerStyles.link}><a href="#">iCloud.com</a></li>
+                            <li className={footerStyles.link}><a>Manage Your Apple Account</a></li>
+                            <li className={footerStyles.link}><a>Apple Store Account</a></li>
+                            <li className={footerStyles.link}><a>iCloud.com</a></li>
                         </ul>
                     </details>
 
                     <details open>
                         <summary className={footerStyles.heading}>Entertainment</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Apple One</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple TV+</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Music</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Arcade</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Podcasts</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Books</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Store</a></li>
+                            <li className={footerStyles.link}><a>Apple One</a></li>
+                            <li className={footerStyles.link}><a>Apple TV+</a></li>
+                            <li className={footerStyles.link}><a>Apple Music</a></li>
+                            <li className={footerStyles.link}><a>Apple Arcade</a></li>
+                            <li className={footerStyles.link}><a>Apple Podcasts</a></li>
+                            <li className={footerStyles.link}><a>Apple Books</a></li>
+                            <li className={footerStyles.link}><a>Apple Store</a></li>
                         </ul>
                     </details>
                 </div>
@@ -75,16 +75,16 @@ const Footer = () => {
                     <details open>
                         <summary className={footerStyles.heading}>Apple Store</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Find a Store</a></li>
-                            <li className={footerStyles.link}><a href="#">Genius Bar</a></li>
-                            <li className={footerStyles.link}><a href="#">Today at Apple</a></li>
-                            <li className={footerStyles.link}><a href="#">Group Reservations</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Camp</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Trade In</a></li>
-                            <li className={footerStyles.link}><a href="#">Ways to Buy</a></li>
-                            <li className={footerStyles.link}><a href="#">Recycling Programme</a></li>
-                            <li className={footerStyles.link}><a href="#">Order Status</a></li>
-                            <li className={footerStyles.link}><a href="#">Shopping Help</a></li>
+                            <li className={footerStyles.link}><a>Find a Store</a></li>
+                            <li className={footerStyles.link}><a>Genius Bar</a></li>
+                            <li className={footerStyles.link}><a>Today at Apple</a></li>
+                            <li className={footerStyles.link}><a>Group Reservations</a></li>
+                            <li className={footerStyles.link}><a>Apple Camp</a></li>
+                            <li className={footerStyles.link}><a>Apple Trade In</a></li>
+                            <li className={footerStyles.link}><a>Ways to Buy</a></li>
+                            <li className={footerStyles.link}><a>Recycling Programme</a></li>
+                            <li className={footerStyles.link}><a>Order Status</a></li>
+                            <li className={footerStyles.link}><a>Shopping Help</a></li>
                         </ul>
                     </details>
                 </div>
@@ -93,26 +93,26 @@ const Footer = () => {
                     <details open>
                         <summary className={footerStyles.heading}>For Business</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Apple and Business</a></li>
-                            <li className={footerStyles.link}><a href="#">Shop for Business</a></li>
+                            <li className={footerStyles.link}><a>Apple and Business</a></li>
+                            <li className={footerStyles.link}><a>Shop for Business</a></li>
                         </ul>
                     </details>
 
                     <details open>
                         <summary className={footerStyles.heading}>For Education</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Apple and Education</a></li>
-                            <li className={footerStyles.link}><a href="#">Shop for Education</a></li>
-                            <li className={footerStyles.link}><a href="#">Shop for University</a></li>
+                            <li className={footerStyles.link}><a>Apple and Education</a></li>
+                            <li className={footerStyles.link}><a>Shop for Education</a></li>
+                            <li className={footerStyles.link}><a>Shop for University</a></li>
                         </ul>
                     </details>
 
                     <details open>
                         <summary className={footerStyles.heading}>For Healthcare</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Apple in Healthcare</a></li>
-                            <li className={footerStyles.link}><a href="#">Mac in Healthcare</a></li>
-                            <li className={footerStyles.link}><a href="#">Health on Apple Watch</a></li>
+                            <li className={footerStyles.link}><a>Apple in Healthcare</a></li>
+                            <li className={footerStyles.link}><a>Mac in Healthcare</a></li>
+                            <li className={footerStyles.link}><a>Health on Apple Watch</a></li>
                         </ul>
                     </details>
                 </div>
@@ -121,24 +121,24 @@ const Footer = () => {
                     <details open>
                         <summary className={footerStyles.heading}>Apple Values</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Accessibility</a></li>
-                            <li className={footerStyles.link}><a href="#">Education</a></li>
-                            <li className={footerStyles.link}><a href="#">Environment</a></li>
-                            <li className={footerStyles.link}><a href="#">Privacy</a></li>
-                            <li className={footerStyles.link}><a href="#">Supply Chain</a></li>
+                            <li className={footerStyles.link}><a>Accessibility</a></li>
+                            <li className={footerStyles.link}><a>Education</a></li>
+                            <li className={footerStyles.link}><a>Environment</a></li>
+                            <li className={footerStyles.link}><a>Privacy</a></li>
+                            <li className={footerStyles.link}><a>Supply Chain</a></li>
                         </ul>
                     </details>
 
                     <details open>
                         <summary className={footerStyles.heading}>About Apple</summary>
                         <ul>
-                            <li className={footerStyles.link}><a href="#">Newsroom</a></li>
-                            <li className={footerStyles.link}><a href="#">Apple Leadership</a></li>
-                            <li className={footerStyles.link}><a href="#">Career Opportunities</a></li>
-                            <li className={footerStyles.link}><a href="#">Investors</a></li>
-                            <li className={footerStyles.link}><a href="#">Ethics and Compliance</a></li>
-                            <li className={footerStyles.link}><a href="#">Events</a></li>
-                            <li className={footerStyles.link}><a href="#">Contact Apple</a></li>
+                            <li className={footerStyles.link}><a>Newsroom</a></li>
+                            <li className={footerStyles.link}><a>Apple Leadership</a></li>
+                            <li className={footerStyles.link}><a>Career Opportunities</a></li>
+                            <li className={footerStyles.link}><a>Investors</a></li>
+                            <li className={footerStyles.link}><a>Ethics and Compliance</a></li>
+                            <li className={footerStyles.link}><a>Events</a></li>
+                            <li className={footerStyles.link}><a>Contact Apple</a></li>
                         </ul>
                     </details>
                 </div>
